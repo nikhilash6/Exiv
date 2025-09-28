92fb9bcd57ebfb3ee47c2cdca7dfc0f1863ab24b
piyushk52 <piyush.kumar.iaf@gmail.com>
2025-09-28 12:10:44 +0530

    simple cli tests added

---
+++ tests/server/test_cli_pkg_flow.py
@@ -0,0 +1,30 @@
+import unittest
+import subprocess
+
+
+SUCCESS_APP_PATH = "tests/test_utils/assets/apps/success_app.py"
+FAILED_APP_PATH = "tests/test_utils/assets/apps/fail_app.py"
+
+class CLITest(unittest.TestCase):
+    def test_cli_run_success(self):
+        result = subprocess.run(
+            ["kirin", "run", SUCCESS_APP_PATH],
+            capture_output=True,
+            text=True,
+            check=False
+        )
+
+        self.assertEqual(result.returncode, 0, "Script should exit with code 0 on success.")
+        self.assertIn("Success script finished.", result.stdout)
+
+    def test_cli_run_fail(self):
+        result = subprocess.run(
+            ["kirin", "run", FAILED_APP_PATH],
+            capture_output=True,
+            text=True,
+            check=False
+        )
+        
+        print("---- result: ", result)
+        self.assertEqual(result.returncode, 0, "Script should exit with code 0 on failure.")
+        self.assertIn("Exception occured:", result.stderr)
