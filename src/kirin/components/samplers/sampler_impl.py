dd71b76e328afaa6d2b364bffd40443f04f1e6ee
piyushk52 <piyush.kumar.iaf@gmail.com>
2025-10-02 22:46:50 +0530

    wip: ksampler

--- src/kirin/components/samplers/sampler_impl.py
+++ src/kirin/components/samplers/sampler_impl.py
@@ -1,3 +1,4 @@
+from typing import Any, Callable, List, Optional
 import torch
 from torch import nn, Tensor
 import torchsde
@@ -6,15 +7,16 @@ from scipy import integrate
 
 import math
 
-from kirin.components.samplers.sampler_types import CONST
-from kirin.model_utils.model_wrapper import ModelOption, ModelWrapper
+from .enum import KSamplerType
+from .sampler_types import CONST
+from ...model_utils.model_wrapper import ModelOption, ModelWrapper
 
 from ...utils.tensor import append_dims
 
-# code adapted from ComfyUI
+# sampler codes adapted from k-diffusion (https://github.com/crowsonkb/k-diffusion)
 
-def ksampler(sampler_name, extra_options={}, inpaint_options={}):
-    if sampler_name == "dpm_fast":
+def ksampler_factory(sampler_name, extra_options={}, inpaint_options={}):
+    if sampler_name == KSamplerType.DPM_FAST.value:
         def dpm_fast_function(model, noise, sigmas, extra_args, callback, disable):
             if len(sigmas) <= 1:
                 return noise
@@ -26,7 +28,7 @@ def ksampler(sampler_name, extra_options={}, inpaint_options={}):
             return sample_dpm_fast(model, noise, sigma_min, sigmas[0], total_steps, extra_args=extra_args, callback=callback, disable=disable)
         sampler_function = dpm_fast_function
     
-    elif sampler_name == "dpm_adaptive":
+    elif sampler_name == KSamplerType.DPM_ADAPTIVE.value:
         def dpm_adaptive_function(model, noise, sigmas, extra_args, callback, disable, **extra_options):
             if len(sigmas) <= 1:
                 return noise
@@ -40,41 +42,50 @@ def ksampler(sampler_name, extra_options={}, inpaint_options={}):
     else:
         sampler_function = globals().get("sample_{}".format(sampler_name))
 
-    return KSAMPLER(sampler_function, extra_options, inpaint_options)
+    return Sampler(sampler_function, extra_options, inpaint_options)
 
-class Sampler:
-    def sample(self):
-        pass
 
-    def max_denoise(self, model_wrap, sigmas):
-        max_sigma = float(model_wrap.inner_model.model_sampling.sigma_max)
-        sigma = float(sigmas[0])
-        return math.isclose(max_sigma, sigma, rel_tol=1e-05) or sigma > max_sigma
-
+# TODO: make a universal __call__ wrapper if this pattern repeats
 class KSamplerX0Inpaint:
     def __init__(self, model, sigmas):
         self.inner_model = model
         self.sigmas = sigmas
+    
     def __call__(self, x, sigma, denoise_mask, model_options={}, seed=None):
         if denoise_mask is not None:
             if "denoise_mask_function" in model_options:
                 denoise_mask = model_options["denoise_mask_function"](sigma, denoise_mask, extra_options={"model": self.inner_model, "sigmas": self.sigmas})
             latent_mask = 1. - denoise_mask
-            x = x * denoise_mask + self.inner_model.inner_model.model_sampling.noise_scaling(sigma.reshape([sigma.shape[0]] + [1] * (len(self.noise.shape) - 1)), self.noise, self.latent_image) * latent_mask
+            x = x * denoise_mask + self.inner_model.model_sampling.noise_scaling(sigma.reshape([sigma.shape[0]] + [1] * (len(self.noise.shape) - 1)), self.noise, self.latent_image) * latent_mask
         out = self.inner_model(x, sigma, model_options=model_options, seed=seed)
         if denoise_mask is not None:
             out = out * denoise_mask + self.latent_image * latent_mask
         return out
 
-class KSAMPLER(Sampler):
-    def __init__(self, sampler_function, extra_options={}, inpaint_options={}):
+class Sampler:
+    def __init__(self, sampler_function: Callable[..., Tensor], extra_options={}, inpaint_options={}):
         self.sampler_function = sampler_function
         self.extra_options = extra_options
         self.inpaint_options = inpaint_options
+        
+    def max_denoise(self, wrapped_model: ModelWrapper, sigmas: List[int]):
+        max_sigma = float(wrapped_model.model_sampling.sigma_max)
+        sigma = float(sigmas[0])
+        return math.isclose(max_sigma, sigma, rel_tol=1e-05) or sigma > max_sigma
 
-    def sample(self, model_wrap, sigmas, extra_args, callback, noise, latent_image=None, denoise_mask=None, disable_pbar=False):
+    def sample(
+        self, 
+        wrapped_model: ModelWrapper, 
+        sigmas: List[int], 
+        extra_args: Any, 
+        callback: Callable[..., Any],
+        noise: Tensor, 
+        latent_image: Optional[Tensor] = None, 
+        denoise_mask: Optional[Tensor] = None, 
+        disable_pbar: bool = False
+    ):
         extra_args["denoise_mask"] = denoise_mask
-        model_k = KSamplerX0Inpaint(model_wrap, sigmas)
+        model_k = KSamplerX0Inpaint(wrapped_model, sigmas)
         model_k.latent_image = latent_image
         if self.inpaint_options.get("random", False): #TODO: Should this be the default?
             generator = torch.manual_seed(extra_args.get("seed", 41) + 1)
@@ -82,7 +93,7 @@ class KSAMPLER(Sampler):
         else:
             model_k.noise = noise
 
-        noise = model_wrap.inner_model.model_sampling.noise_scaling(sigmas[0], noise, latent_image, self.max_denoise(model_wrap, sigmas))
+        noise = wrapped_model.model_sampling.noise_scaling(sigmas[0], noise, latent_image, self.max_denoise(wrapped_model, sigmas))
 
         k_callback = None
         total_steps = len(sigmas) - 1
@@ -90,7 +101,7 @@ class KSAMPLER(Sampler):
             k_callback = lambda x: callback(x["i"], x["denoised"], x["x"], total_steps)
 
         samples = self.sampler_function(model_k, noise, sigmas, extra_args=extra_args, callback=k_callback, disable=disable_pbar, **self.extra_options)
-        samples = model_wrap.inner_model.model_sampling.inverse_noise_scaling(sigmas[-1], samples)
+        samples = wrapped_model.model_sampling.inverse_noise_scaling(sigmas[-1], samples)
         return samples
 
 def to_d(x: Tensor, sigma: int, denoised: Tensor):
